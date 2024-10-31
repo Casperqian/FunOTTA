8f2feb84614d1b214bb71c7316994feb49c96036
Qian Zeng <142384657+Casperqian@users.noreply.github.com>
2024-10-31 17:03:10 +0800

    Create algorithms.py

---
+++ domainbed/algorithms.py
@@ -0,0 +1,102 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+from tqdm import tqdm
+import copy
+import numpy as np
+import itertools
+
+from domainbed import networks
+from domainbed.lib.misc import random_pairs_of_minibatches
+
+
+ALGORITHMS = [
+    'ERM',
+]
+
+
+def get_algorithm_class(algorithm_name):
+    """Return the algorithm class with the given name."""
+    if algorithm_name not in globals():
+        raise NotImplementedError(
+            "Algorithm not found: {}".format(algorithm_name))
+    return globals()[algorithm_name]
+
+
+class Algorithm(torch.nn.Module):
+    """
+    A subclass of Algorithm implements a domain generalization algorithm.
+    Subclasses should implement the following:
+    - update()
+    - predict()
+    """
+
+    def __init__(self, input_shape, num_classes, num_domains, hparams):
+        super(Algorithm, self).__init__()
+        self.hparams = hparams
+
+    def update(self, minibatches, unlabeled=None):
+        """
+        Perform one update step, given a list of (x, y) tuples for all
+        environments.
+
+        Admits an optional list of unlabeled minibatches from the test domains,
+        when task is domain_adaptation.
+        """
+        raise NotImplementedError
+
+    def predict(self, x):
+        raise NotImplementedError
+
+
+class ERM(Algorithm):
+    """
+    Empirical Risk Minimization (ERM)
+    """
+
+    def __init__(self, input_shape, num_classes, num_domains, steps, hparams):
+        super(ERM, self).__init__(input_shape, num_classes, num_domains,
+                                  hparams)
+        self.featurizer = networks.Featurizer(input_shape, self.hparams)
+        self.classifier = networks.Classifier(
+            self.featurizer.n_outputs, num_classes,
+            self.hparams['nonlinear_classifier'])
+
+        self.optimizer = torch.optim.Adam(
+            itertools.chain(self.featurizer.parameters(),
+                            self.classifier.parameters()),
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams['weight_decay'])
+
+        if steps > 0:
+            self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+                self.optimizer, T_max=steps)
+
+        self.criterion = nn.CrossEntropyLoss()
+
+        # self.fp, self.yp, self.lp = None, None, None
+        # self.register_buffer('fp', None)
+        # self.register_buffer('yp', None)
+        # self.register_buffer('lp', None)
+
+    def update(self, minibatches, unlabeled=None):
+        all_x = torch.cat([x for x, y in minibatches])
+        all_y = torch.cat([y for x, y in minibatches])
+        loss = self.criterion(self.predict(all_x), all_y)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.scheduler.step()
+        return {'loss': loss.item()}
+
+    def predict(self, x):
+        x = self.featurizer(x)
+        x = self.classifier(x)
+        return x
+
+    def forward(self, x):
+        return self.predict(x)
