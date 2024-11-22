e8b5b9b58542ddb6394a77c7942fd79c1e652ed4
Qian Zeng <142384657+Casperqian@users.noreply.github.com>
2024-11-22 14:36:51 +0800

    Update adaptation.sh

--- adaptation.sh
+++ adaptation.sh
@@ -1,13 +1,25 @@
-export CUDA_VISIBLE_DEVICES=0
+# Function to adapt the source model with different algorithms
 
-INPUT_DIR="./checkpoint/MultipleEnvironmentDR/resnet50-BN/trial_seed-0/"
-ADAPT_ALGORITHMS=("TentClf" "PLClf" "SHOT" "T3A" "TAST" "UniDG" "DeYOClf" "Ours")
+run_model() {
+    local TASK=$1
+    local CONFIG=$2
+    local CUDA_DEVICE=$3
+    local INPUT_DIR="./checkpoint/${TASK}/${CONFIG}/trial_seed-0/"
+    local ADAPT_ALGORITHMS=("TentClf" "PLClf" "SHOT" "T3A" "TAST" "UniDG" "DeYOClf" "Ours")
 
-for algorithm in "${ADAPT_ALGORITHMS[@]}"; do
-    echo "Running $algorithm..."
-    python -m domainbed.scripts.unsupervised_adaptation \
-        --input_dir=$INPUT_DIR \
-        --adapt_algorithm=$algorithm
-done
+    echo "Setting CUDA_VISIBLE_DEVICES=$CUDA_DEVICE"
+    export CUDA_VISIBLE_DEVICES=$CUDA_DEVICE
 
-echo "All algorithms have been run."
+    echo "Running task: $TASK with configuration: $CONFIG"
+    for algorithm in "${ADAPT_ALGORITHMS[@]}"; do
+        echo "Running $algorithm..."
+        python -m domainbed.scripts.unsupervised_adaptation \
+            --input_dir=$INPUT_DIR \
+            --adapt_algorithm=$algorithm
+    done
+    echo "Finished running task: $TASK with configuration: $CONFIG"
+}
+
+run_model "MultipleEnvironmentDR" "resnet50-BN" 0  
+
+echo "All tasks, configurations, and algorithms have been run."
